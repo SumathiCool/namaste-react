88da393799c85780b1a725a9513fd78a806a2766
SumathiCool <sumathimari1998@gmail.com>
2023-12-22 12:57:04 +0530

    Episode 1

---
+++ EP1/App.js
@@ -0,0 +1,30 @@
+// {} - add attribute to the element
+
+// const heading = React.createElement("h1",{id: "head"},"Hello World from React!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+// add one more div and one single child
+
+// const parent = React.createElement("div" , {id: "parent"} , 
+// React.createElement("div" , {id: "child"} ,
+// React.createElement("h1", {id:"heading"} , "I am h1 tag")
+// )
+// );
+
+// add sibling tag - using array bracket
+
+const parent = React.createElement("div" , {id: "parent"} , 
+React.createElement("div" , {id: "child"} ,
+[React.createElement("h1", {id:"heading"} , "I am h1 tag") , React.createElement("h2", {id:"heading"} , "I am h2 tag")]
+)
+);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+
+
+
